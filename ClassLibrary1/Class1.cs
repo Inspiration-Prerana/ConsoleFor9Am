cf2c171a3939f6e7f48f98ad1796090256c27b6a
speedchandu <Chandan.Bhagat@outlook.com>
2016-03-20 11:54:10 +0545

    Initiall Commit

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+using ClassLibrary1.Math;
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public int i;
+        private int j;
+
+        //Constructor
+        public Class1()
+        {
+            i = 0;
+            j = 0;
+        }
+
+        public Class1( int i)
+        {
+            this.i = i;
+            this.j = 0;
+        }
+
+        public Class1(int i, int j)
+        {
+            this.i = i;
+            this.j = j;
+        }
+
+        //Methods
+        public int Add()
+        {
+            //statements
+            return i + j;
+        } 
+        //Destructors
+    }
+
+}
---
+++ ClassLibrary1/Class2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    class Class2
+    {
+        Class1 c1 = new Class1();
+        Class1 c2 = new Class1(5);
+        Class1 c3 = new Class1(5,2);
+
+        public Class2()
+        {
+            
+        }
+    }
+}
